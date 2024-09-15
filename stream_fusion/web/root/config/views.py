53f1ef4f63c978a21108e93f4834af0540483745
LimeDrive <limecat@limedrive.eu>
2024-09-15 16:17:52 +0200

    Push logo to imgur

--- stream_fusion/web/root/config/views.py
+++ stream_fusion/web/root/config/views.py
@@ -1,11 +1,11 @@
 from cachetools import TTLCache
 from fastapi import APIRouter, Request
-from fastapi.responses import  RedirectResponse
+from fastapi.responses import  FileResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 from stream_fusion.logging_config import logger
 from stream_fusion.version import get_version
-from stream_fusion.web.root.config.schemas import ManifestResponse
+from stream_fusion.web.root.config.schemas import ManifestResponse, StaticFileResponse
 from stream_fusion.settings import settings
 
 router = APIRouter()
@@ -30,7 +30,7 @@ async def configure(request: Request):
 # @router.get("/static/{file_path:path}", response_model=StaticFileResponse)
 # async def serve_static(file_path: str):
 #     logger.debug(f"Serving static file: {file_path}")
-#     return FileResponse(f"/app/stream_fusion/templates/{file_path}")
+#     return FileResponse(f"/app/stream_fusion/static/{file_path}")
 
 
 @router.get("/manifest.json")
@@ -38,7 +38,7 @@ async def get_manifest():
     logger.info("Serving manifest.json")
     return ManifestResponse(
         id="community.limedrive.streamfusion",
-        icon="https://raw.githubusercontent.com/LimeDrive/stream-fusion/limedrive-add-auth/stream_fusion/static/logo-stream-fusion.png",
+        icon="https://i.imgur.com/q2VSdSp.png",
         version=str(get_version()),
         resources=[
             'catalog',
@@ -101,7 +101,7 @@ async def get_manifest():
     logger.info("Serving manifest.json")
     return ManifestResponse(
         id="community.limedrive.streamfusion",
-        icon="https://raw.githubusercontent.com/LimeDrive/stream-fusion/limedrive-add-auth/stream_fusion/static/logo-stream-fusion.png",
+        icon="https://i.imgur.com/q2VSdSp.png",
         version=str(get_version()),
         resources=[
             'catalog',
